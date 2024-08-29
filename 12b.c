cd40ee752ff534a74f2a1a20653db4bab490986d
Kunwar Yuvraj <63793259+Kunwar-Yuvraj@users.noreply.github.com>
2024-08-30 02:09:08 +0530

    👍

--- 12b.c
+++ 12b.c
@@ -3,89 +3,98 @@
 #include <limits.h>
 #include <time.h>
 
-int n, opcount, dist[100][100];
+int dist[100][100];
+int n;
+int count;
 
-int floyds(int adjMat[n][n], int n)
-{
-    opcount = 0;
+void floyds(int adj[n][n], int n){
+    count = 0;
+
+    // init dist
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            if (adjMat[i][j] == -1)
-                dist[i][j] = INT_MAX;
-            else
-                dist[i][j] = adjMat[i][j];
-    for (int k = 0; k < n; k++)
-        for (int i = 0; i < n; i++)
-        {
-            int tempDist = dist[i][k];
-            for (int j = 0; j < n; j++)
-            {
-                if (dist[i][j] > tempDist)
-                {
-                    opcount++;
-                    if (dist[k][j] != INT_MAX && dist[i][j] > tempDist + dist[k][j])
-                    {
-                        dist[i][j] = tempDist + dist[k][j];
+            if (adj[i][j] == -1) dist[i][j] = INT_MAX;
+            else dist[i][j] = adj[i][j];
+
+    // floyd-warshalls algo
+    for (int k = 0; k < n; k++){
+        for (int i = 0; i < n; i++){
+            int tmp = dist[i][k];
+            for (int j = 0; j < n; j++){
+                if (dist[i][j] > tmp){
+                    count++;
+                    if (dist[k][j] != INT_MAX && dist[i][j] > tmp + dist[k][j]){
+                        dist[i][j] = tmp + dist[k][j];
                     }
                 }
             }
         }
+    }
 }
 
-void tester()
-{
-    printf("Enter the number of nodes: ");
+void tester(){
+    printf("Enter numbe of nodes: ");
     scanf("%d", &n);
-    int adjMat[n][n];
-    printf("Enter adjacency matrix: \n");
-    printf("[Note: Enter -1 to indicate infinity]\n");
+    
+    printf("Enter adjacency matrix:\n");
+    int adj[n][n];
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            scanf("%d", &adjMat[i][j]);
-    floyds(adjMat, n);
-    printf("Shortest distance between all pair of nodes:\n");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-            printf("%d\t", dist[i][j]);
+            scanf("%d", &adj[i][j]);
+
+    floyds(adj, n);
+    printf("Shortest path between every pair:\n");
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (dist[i][j] == INT_MAX) printf("INF ");
+            else printf("%d ", dist[i][j]);
+        }
         printf("\n");
     }
 }
 
-void plotter()
-{
+void writeFiles(const char *bestFile, const char *worstFile, void (*shortestPath)(int a[n][n], int)){
+    FILE *fb, *fw;
+    fb = fopen(bestFile, "w");
+    fw = fopen(worstFile, "w");
+
     srand(time(NULL));
-    FILE *fp = fopen("floydes.txt", "w");
-    for (int k = 2; k < 12; k++)
-    {
+
+    for (int k = 2; k < 12; k++){
         n = k;
-        int adjMat[n][n];
+        int adj[n][n];
+        // best case: fully connected with minimal weight
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                if (i != j)
-                    adjMat[i][j] = rand();
-                else
-                    adjMat[i][j] = 0;
-        floyds(adjMat, n);
-        fprintf(fp, "%d\t%d\n", n, opcount);
+                adj[i][j] = (i == j)? 0: 1;
+        shortestPath(adj, n);
+        fprintf(fb, "%d\t%d\n", n, count);
+        
+        // worst case: connected graph with random weights
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                adj[i][j] = (i == j)? 0: rand() + 100 + 1;
+        shortestPath(adj, n);
+        fprintf(fw, "%d\t%d\n", n, count);
     }
-    fclose(fp);
+    fclose(fb);
+    fclose(fw);
 }
 
-void main()
-{
-    int ch;
-    printf("Enter\n1.Tester\n2.Plotter\n");
-    scanf("%d", &ch);
-    switch (ch)
-    {
-    case 1:
-        tester();
-        break;
-    case 2:
-        plotter();
-        break;
-    default:
-        printf("Invalid choice!! ");
-    }
+void plotter(){
+    printf("Generating files...!\n");
+    writeFiles("best-floyds.txt", "worst-floyds.txt", floyds);
+    printf("Files generated successfully!\n");
+}
+
+int main(){
+    printf("1. Tester\n2. Plotter\n");
+    printf("Enter your choice: ");
+    int choice; scanf("%d", &choice);
+
+    if (choice == 1) tester();
+    else if (choice == 2) plotter();
+    else printf("Invalid choice!\n");
+    
+    return 0;
 }
