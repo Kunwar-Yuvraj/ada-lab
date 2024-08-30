7bb368d347571589de54ae7293f815bcfdaf79b6
Kunwar Yuvraj <63793259+Kunwar-Yuvraj@users.noreply.github.com>
2024-08-30 17:13:17 +0530

    👍

--- 10.c
+++ 10.c
@@ -1,124 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int n, indeg[20], opcount = 0, queue[20], front = -1, rear = -1;
+#define max 20
 
-int bfs(int mat[n][n])
-{
-    int count = 0;
-    front = rear = -1;
+int n, count = 0;
+int f, r;
+int q[max], in[max];
+
+int bfs(int mat[n][n]){
+    f = r = -1;
+    int xm = 0;
 
     for (int i = 0; i < n; i++)
-    {
-        if (indeg[i] == 0)
-        {
-            queue[++rear] = i;
-        }
-    }
+        if(in[i] == 0)
+            q[++r] = i;
 
-    while (front != rear)
-    {
-        int curr = queue[++front];
-        count++;
+    while (f != r){
+        int cur = q[++f];
+        xm++;
 
-        for (int i = 0; i < n; i++)
-        {
-            opcount++;
-            if (mat[curr][i])
-            {
-                indeg[i]--;
-                if (!indeg[i])
-                    queue[++rear] = i;
+        for (int i = 0; i < n; i++){
+            count++;
+
+            if (mat[cur][i]){
+                in[i]--;
+                if (!in[i]) q[++r] = i;
             }
         }
     }
-
-    return count != n;
+    return xm != n;
 }
 
-void tester()
-{
-    printf("Enter number of vertices :\n");
-    scanf("%d", &n);
-    int adjMat[n][n];
+void tester(){
+    printf("Enter number of vertices: "); scanf("%d", &n);
 
-    for (int i = 0; i < n; i++)
-        indeg[i] = 0;
+    for (int i = 0; i < n; i++) in[i] = 0;
 
-    printf("Enter the adjacency matrix :\n");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            scanf("%d", &adjMat[i][j]);
-            if (adjMat[i][j])
-                indeg[j]++;
+    int mat[n][n];
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            scanf("%d", &mat[i][j]);
+            if (mat[i][j]) in[j]++;
         }
     }
-
-    if (bfs(adjMat))
-    {
-        printf("Cycle exists..Cannot perform topological sorting!!!");
+    if (bfs(mat)){
+        printf("Cycle detected. Can't generated topological order!\n");
         exit(0);
-    }
-    else
-    {
-        printf("Topological sorting order : \n");
-
-        for (int i = 0; i <= rear; i++)
-        {
-            printf("%d ", queue[i]);
-        }
+    } else {
+        printf("Topological order: ");
+        for (int i = 0; i < n; i++) printf("%d ", q[i]);
     }
 }
 
-void plotter()
-{
-    FILE *f1 = fopen("srcrmMatTopSort.txt", "w");
-
-    for (int k = 1; k <= 10; k++)
-    {
+void plotter(){
+    printf("Writing file...\n");
+    FILE *f = fopen("top-bfs.txt", "w");
+    for (int k = 1; k <= 10; k++){
         n = k;
-        int adjMat[n][n];
-
-        for (int i = 0; i < n; i++)
-            indeg[i] = 0;
+        int mat[n][n];
+        for (int i = 0; i < n; i++) in[i] = 0;
 
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                adjMat[i][j] = 0;
+                mat[i][j] = 0;
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                adjMat[i][j] = 1;
-                indeg[j]++;
+        for (int i = 0; i < n - 1; i++){
+            for (int j = i + 1; j < n; j++){
+                mat[i][j] = 1;
+                in[j]++;
             }
         }
-
-        opcount = 0;
-        bfs(adjMat);
-        fprintf(f1, "%d\t%d\n", n, opcount);
+        count = 0;
+        bfs(mat);
+        fprintf(f, "%d\t%d\n", n, count);
     }
-
-    fclose(f1);
+    fclose(f);
+    printf("File written successfully!\n");
 }
 
-void main()
-{
-    int choice;
-    printf("Enter\n1.Tester\n2.Plotter\n");
-    scanf("%d", &choice);
-    switch (choice)
-    {
-    case 1:
-        tester();
-        break;
-    case 2:
-        plotter();
-        break;
-    default:
-        printf("Invalid choice");
-    }
+int main(){
+    printf("1. Tester\n2. Plotter\n");
+    printf("Enter your choice: ");
+    int choice; scanf("%d", &choice);
+
+    if (choice == 1) tester();
+    else if (choice == 2) plotter();
+    else printf("Invalid choice!\n");
+    
+    return 0;
 }
